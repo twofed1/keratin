46fd9258d77b0376f8fe103bde5d32840e42352e
rom <romula@gmail.com>
2018-11-10 19:51:37 +0300

    add product page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  /*menu*/
   var burger = $('.menu__btn'),
       menu = $('.menu__wrapper');
   
@@ -11,10 +12,30 @@ $(document).ready(function(){
       menu.addClass('active');
     }      
   })
-})
-
-$(document).ready(function() {
   
+  
+  /*dropdown*/
+  $('.caret').on('click', function(){
+    var parent = $(this).closest('.dropdown'),
+        items = parent.find('.dropdown__item');
+
+    if(parent.hasClass('open')){
+      return parent.removeClass('open');
+    }
+    
+    parent.addClass('open');
+
+    items.on('click', function(){
+      items.removeClass('active');
+      $(this).addClass('active');
+
+      parent.removeClass('open');
+      parent.find('.dropdown__list').append(items.sort(el => $(el).hasClass('active') ? -1 : 1));
+    })
+  });
+  
+  
+  /*tab function*/
   function tabInit(parent){
     var header = $(parent).find('.tabs__header'),
         titles = header.find('.tabs__btn'),
@@ -53,20 +74,30 @@ $(document).ready(function() {
     
   }
   
+  /*init tabs*/
   tabInit('.tabs');
-})
-
-$('.caret').on('click', function(){
-  var parent = $(this).closest('.dropdown'),
-      items = parent.find('.dropdown__item');
   
-  parent.addClass('open');
   
-  items.on('click', function(){
-    items.removeClass('active');
-    $(this).addClass('active');
-    
-    parent.removeClass('open');
-    parent.find('.dropdown__list').append(items.sort(el => $(el).hasClass('active') ? -1 : 1));
+  /*counter*/
+  var add = $('.counter').find('.add'),
+      ded = $('.counter').find('.ded'),
+      count = $('.counter').find('.count')[0],
+      value = Number($('.counter').find('.count')[0].innerText);
+  
+  add.on('click', function(){
+    value = value + 1;
+    $(count).text(value);
   })
+  
+  ded.on('click', function(){    
+    value = value > 0 ?  value - 1 : 0;
+    $(count).text(value);
+  })
+  
+  
+  /*volume change*/
+  $('.volume__item').on('click', function(){
+    $(this).siblings('.volume__item').removeClass('active');
+    $(this).addClass('active');
+  });
 })
